d317478c9e25184ae12fbdc3bc0b144787935b78
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2020-03-21 00:24:35 +0530

    Updates

---
+++ precache-manifest.8f8ca2783f5723d9951a8f7b3705c868.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d36efaa494092afc5161083a2cbc76f",
+    "url": "/OrcaDashboard/index.html"
+  },
+  {
+    "revision": "38d789a25e0d02335731",
+    "url": "/OrcaDashboard/static/css/2.9c5146eb.chunk.css"
+  },
+  {
+    "revision": "bd8dfad1ff0ee3e3c6a0",
+    "url": "/OrcaDashboard/static/css/main.3633a8ad.chunk.css"
+  },
+  {
+    "revision": "38d789a25e0d02335731",
+    "url": "/OrcaDashboard/static/js/2.e03f0914.chunk.js"
+  },
+  {
+    "revision": "907125713a565290e8273b2dc6d2f9e6",
+    "url": "/OrcaDashboard/static/js/2.e03f0914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8dfad1ff0ee3e3c6a0",
+    "url": "/OrcaDashboard/static/js/main.8497a26e.chunk.js"
+  },
+  {
+    "revision": "54e9e679c811e70e49f6",
+    "url": "/OrcaDashboard/static/js/runtime-main.3248cb2e.js"
+  },
+  {
+    "revision": "6baed2bf580964bec9559ad83caee43d",
+    "url": "/OrcaDashboard/static/media/CircularStd-Bold.6baed2bf.otf"
+  },
+  {
+    "revision": "6365c40aa59d462f1cc52ccce9635cb4",
+    "url": "/OrcaDashboard/static/media/CircularStd-Book.6365c40a.otf"
+  },
+  {
+    "revision": "4fcdd97fadc3a1d9887f816f2aa67f1d",
+    "url": "/OrcaDashboard/static/media/CircularStd-Medium.4fcdd97f.otf"
+  },
+  {
+    "revision": "be2ed1511f856f57648ebb4d1c0a1b96",
+    "url": "/OrcaDashboard/static/media/user-img-placeholder.be2ed151.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/OrcaDashboard/precache-manifest.8f8ca2783f5723d9951a8f7b3705c868.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OrcaDashboard/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
